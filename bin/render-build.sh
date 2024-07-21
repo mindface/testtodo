edcc19ff8cf92c888742ef741b9454298c89a1dd
mindface <in00und@gmail.com>
2024-07-21 21:51:05 +0900

    settings shell

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,4 @@ bundle install
 
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec ridgepole -c config/database.yml -E production db:migrate
+bundle exec rake db:migrate
